e24dc6676b5c2b829c940ee0d4297a8404a0fb61
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-03-11 16:33:11 +0100

    1

---
+++ ExoRobiSite/Controllers/RefController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExoRobiSite.Controllers
+{
+    public class RefController : Controller
+    {
+        // GET: Ref
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
